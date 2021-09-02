4b1928b75119919858c414dd061f0a3bae92ee3b
zhangbingbing <lookingatstarts@163.com>
2021-09-03 07:12:22 +0800

    时间转换工具添加新方法

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # easyUtils
 一些简单的工具
+
+- com.easyutils.limiter.RateLimiter 利用redis-cl实现的限流器
+- com.easyutils.time.DateTimeConverter 时间时区转换工具
--- src/main/java/com/easyutils/time/DateTimeConverter.java
+++ src/main/java/com/easyutils/time/DateTimeConverter.java
@@ -9,27 +9,48 @@ import java.util.TimeZone;
 /**
  * 时区转换工具
  * 1、LocalDateTime = LocalDate +  LocalTime  仅仅表示时间
+ * LocalDateTime.now(); 获取当前时间戳后，使用系统默认时区格式化时间
  * eg:2021-08-10 80:22:10 可以认为它是北京时间，也可以认为它是伦敦时间，没有明确的时区
+ * <p>
  * 2、时间戳与时间的关系
  * 时间戳没有时区概念，时间是有时区概念
  * 时间戳是一个不断递增的整数，是距离格林威治时间过去的秒数
  * 时间与时间戳是N:1关系
  * 时间可以认为是同一个时间戳不同显示 eg:1630590333 北京时间：2021-09-02 21:45:33 伦敦时间：2021-09-02 14:45:33
+ * 如果没有手动指定时区，通过某个类能获取到时间，那么它底层一定使用了默认时区，获取系统当前时区
+ * <p>
  * 3、ZonedDateTime = LocalDateTime + ZoneId 时间+时区 才能转换成时间戳
+ * <p>
  * 4、时区
  * 光靠一个时间无法确定一个时刻(时间戳)，需要给当地时间加上一个时区
- * 5、java.util.Date实际上存储了一个long类型毫秒表示的时间戳
+ * 6、java8之前的api: TimeZone时区 Date时间戳，没有时区 Calendar时间戳+时区
+ * java.util.Date实际上存储了一个long类型毫秒表示的时间戳
+ * 获取日期时间时，使用系统时区
  * getYear获取的年份要加上1900
  * getMonth获取的月份要加上1
  * getDate获取天数不要加1
- * 使用SimpleDateFormat格式化时间
- * 5、在数据库中存储时间戳时，尽量使用long型时间戳，它具有省空间，效率高，不依赖数据库的优点
+ * <p>
+ * 使用SimpleDateFormat格式化时间，可以先设置时区
+ * <p>
+ * java.util.Calendar
+ * Calendar可以设置时区和时间戳
+ * <p>
+ * 5、夏令时
+ * 使用洲/城市方式表示时区时，会有夏令时问题
+ * <p>
+ * 6、在数据库中存储时间戳时，尽量使用long型时间戳，它具有省空间，效率高，不依赖数据库的优点
  */
 public class DateTimeConverter {
 
     public static final String YEAR_MONTH_DAY_TIME_FORMATTER = "yyyy-MM-dd HH:mm:ss";
     public static final String YEAR_MONTH_DAY_FORMATTER = "yyyy-MM-dd";
+    /**
+     * 东八区
+     */
     public static final ZoneId DEFAULT_ZONE_ID = ZoneId.ofOffset("UTC", ZoneOffset.ofHours(8));
+    /**
+     * 格林威治标准时区
+     */
     public static final ZoneId UTC_ZONE_ID = ZoneId.of("UTC");
 
     /**
@@ -55,6 +76,26 @@ public class DateTimeConverter {
         return LocalDate.parse(time, DateTimeFormatter.ofPattern(formatPattern));
     }
 
+    public static String format(Date date, TimeZone timeZone, String formatPattern) {
+        return ZonedDateTime.ofInstant(date.toInstant(), timeZone.toZoneId())
+                .toLocalDateTime()
+                .format(DateTimeFormatter.ofPattern(formatPattern));
+    }
+
+    public static String format(LocalDateTime localDateTime, ZoneId zoneId, String formatPattern) {
+        return localDateTime.atZone(zoneId)
+                .toLocalDateTime()
+                .format(DateTimeFormatter.ofPattern(formatPattern));
+    }
+
+    public static ZoneId toZoneId(TimeZone timeZone) {
+        return timeZone.toZoneId();
+    }
+
+    public static TimeZone toTimeZone(ZoneId zoneId) {
+        return TimeZone.getTimeZone(zoneId);
+    }
+
     /**
      * 时区：北京时间
      * 当前时间
@@ -71,8 +112,7 @@ public class DateTimeConverter {
      * 时间在不同时区转换
      */
     public static ZonedDateTime convert(LocalDateTime localDateTime, ZoneId from, ZoneId to) {
-        return withZone(localDateTime, from)
-                .withZoneSameInstant(to);
+        return localDateTime.atZone(from).withZoneSameInstant(to);
     }
 
     public static ZonedDateTime convert(Calendar calendar, ZoneId to) {
@@ -101,8 +141,7 @@ public class DateTimeConverter {
      * 时间在不同时区转换
      */
     public static LocalDateTime convertToLocalDateTime(LocalDateTime localDateTime, ZoneId from, ZoneId to) {
-        // toLocalDateTime会丢失时区信息
-        return convert(localDateTime, from, to).toLocalDateTime();
+        return localDateTime.atZone(from).withZoneSameInstant(to).toLocalDateTime();
     }
 
     /**
@@ -123,7 +162,7 @@ public class DateTimeConverter {
      * 日期转时间戳(毫秒)
      */
     public static long convertToSecond(LocalDateTime localDateTime, ZoneId zoneId) {
-        return convertToMilli(localDateTime, zoneId) / 1000;
+        return localDateTime.atZone(zoneId).toInstant().toEpochMilli() / 1000;
     }
 
     /**
